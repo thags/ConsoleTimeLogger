cd133ad6192a400b634d763ca35e861c9f6b51ab
Tyler Hagstrom <tyler.hagstrom@gmail.com>
2021-11-08 16:50:52 -0600

    Set up some basic SQLite mostly to just start testing things out. The app will now create or open a sqlite db, then try to create a time table, insert a test record and console the records out.

--- ConsoleTimeLogger/Program.cs
+++ ConsoleTimeLogger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Data.Sqlite;
 
 namespace ConsoleTimeLogger
 {
@@ -6,7 +7,42 @@ namespace ConsoleTimeLogger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var connection = new SqliteConnection("Data Source=time.db");
+            connection.Open();
+
+
+            //try to create a table
+            try
+            {
+                var createTable = connection.CreateCommand();
+                createTable.CommandText = "CREATE TABLE time(name VARCHAR(50));";
+                createTable.ExecuteNonQuery();
+            } 
+            //if the table is already there it will notify of that, just for testing purposes, but will continue on to the rest of the code. 
+            catch
+            {
+                Console.WriteLine("table already exists, continuing...");
+            }
+            
+
+            //insert records
+            var transaction = connection.BeginTransaction();
+            var insertCmd = connection.CreateCommand();
+            insertCmd.CommandText = "INSERT INTO time VALUES('test')";
+            insertCmd.ExecuteNonQuery();
+
+            transaction.Commit();
+
+            //read records
+            var selectCmd = connection.CreateCommand();
+            selectCmd.CommandText = "SELECT * FROM time";
+            using (var reader = selectCmd.ExecuteReader()) {
+                while (reader.Read())
+                {
+                    var result = reader.GetString(0);
+                    Console.WriteLine(result);
+                }
+            }
         }
     }
 }
