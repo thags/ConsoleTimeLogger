c1e108b96cad907dc30f5c2b5b3df47d2ce83ada
Tyler Hagstrom <tyler.hagstrom@gmail.com>
2021-11-09 07:33:38 -0600

    InsertHours method created to INSERT hours based on user input into the DB. The app also now loops asking for user input until a "0" is inputted.

--- ConsoleTimeLogger/Program.cs
+++ ConsoleTimeLogger/Program.cs
@@ -5,43 +5,78 @@ namespace ConsoleTimeLogger
 {
     class Program
     {
+        public static string getDay()
+        {
+            DateTime today = DateTime.Today;
+            return today.ToString("ddMMyyyy");
+        }
+        static void InsertHours(int hours, Microsoft.Data.Sqlite.SqliteConnection connection)
+        {
+            if(hours > 0)
+            {
+                var transaction = connection.BeginTransaction();
+                var insertCmd = connection.CreateCommand();
+                insertCmd.CommandText = $"INSERT INTO time(hours, date) VALUES({hours}, {getDay()})";
+                insertCmd.ExecuteNonQuery();
+
+                transaction.Commit();
+            }
+        }
         static void Main(string[] args)
         {
             using var connection = new SqliteConnection("Data Source=time.db");
             connection.Open();
-
+            
+            Console.WriteLine(getDay());
 
             //try to create a table
             try
             {
                 var createTable = connection.CreateCommand();
-                createTable.CommandText = "CREATE TABLE time(name VARCHAR(50));";
+                createTable.CommandText = @"CREATE TABLE time(hours INT, 
+                                                          date DATE
+                                                                    );";
                 createTable.ExecuteNonQuery();
-            } 
-            //if the table is already there it will notify of that, just for testing purposes, but will continue on to the rest of the code. 
+            }
             catch
             {
-                Console.WriteLine("table already exists, continuing...");
+                Console.WriteLine("Table already exists");
             }
-            
+
+            int userInput = 1;
+            while (userInput != 0)
+            {
+                Console.WriteLine("Input your command");
+                Console.WriteLine("0 to exit");
+                Console.WriteLine("Insert any number to add that many hours to todays coding time");
+                string newInput = Console.ReadLine();
+                userInput = Convert.ToInt16(newInput);
+                InsertHours(userInput, connection);
+                
+            }
+
 
             //insert records
+            
+            /*
             var transaction = connection.BeginTransaction();
             var insertCmd = connection.CreateCommand();
-            insertCmd.CommandText = "INSERT INTO time VALUES('test')";
+            insertCmd.CommandText = $"INSERT INTO time(hours, date) VALUES('1', {getDay()})";
             insertCmd.ExecuteNonQuery();
 
             transaction.Commit();
+            */
 
             //read records
             var selectCmd = connection.CreateCommand();
-            selectCmd.CommandText = "SELECT * FROM time";
-            using (var reader = selectCmd.ExecuteReader()) {
-                while (reader.Read())
-                {
-                    var result = reader.GetString(0);
-                    Console.WriteLine(result);
-                }
+            selectCmd.CommandText = $"SELECT * FROM time";
+            using var reader = selectCmd.ExecuteReader(); 
+            while (reader.Read())
+            {
+                var first = reader.GetString(0);
+                var second = reader.GetString(1);
+                Console.WriteLine(first);
+                Console.WriteLine(second);
             }
         }
     }
